3c1eca746eebe1f0b3edee2939308870609dd641
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-04-05 15:53:49 +0300

    Inventory and logo

--- dev/src/bussines_layer/Inventory.java
+++ dev/src/bussines_layer/Inventory.java
@@ -1,4 +1,15 @@
 package bussines_layer;
 
 public class Inventory {
+    //fields
+    CategoryController categoryController;
+    ProductController productController;
+    ReportController reportController;
+
+    //Constructors
+    public Inventory() {
+        this.categoryController=new CategoryController(new GIDAllocator());
+        this.productController = new ProductController(new PIDAllocator());
+        this.reportController = new ReportController();
+    }
 }
--- dev/src/presentation_layer/CLController.java
+++ dev/src/presentation_layer/CLController.java
@@ -1,4 +1,27 @@
 package presentation_layer;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+
 public class CLController {
+    public static void main(String[] args)
+    {
+        printLogo();
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter first number- ");
+
+
+    }
+    static private void printLogo(){
+        String logo =   " _____                       _     _ \n" +
+                        "/  ___|                     | |   (_)\n" +
+                        "\\ `--. _   _ _ __   ___ _ __| |    _ \n" +
+                        " `--. \\ | | | '_ \\ / _ \\ '__| |   | |\n" +
+                        "/\\__/ / |_| | |_) |  __/ |  | |___| |\n" +
+                        "\\____/ \\__,_| .__/ \\___|_|  \\_____/_|\n" +
+                        "            | |                      \n" +
+                        "            |_|     ";
+        System.out.println(logo);
+    }
 }
