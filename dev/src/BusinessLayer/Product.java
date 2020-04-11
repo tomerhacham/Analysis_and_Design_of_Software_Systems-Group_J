be895e9439c62d5e44d39832bf36ee01815750d1
Shira Sarig <sarigshi@post.bgu.ac.il>
2020-04-11 13:04:57 +0300

    Product implementation

--- dev/src/BusinessLayer/Product.java
+++ dev/src/BusinessLayer/Product.java
@@ -1,4 +1,20 @@
 package BusinessLayer;
 
 public class Product {
+
+    private int ID;
+    private int weight;
+    private int quantity;
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
