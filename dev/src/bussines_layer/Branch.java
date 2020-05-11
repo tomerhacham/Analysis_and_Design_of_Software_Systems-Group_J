e0882119bbdf174e496512a856671f7aef737185
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-05-11 10:43:32 +0300

    create persistence layer. integrate both inventory and suppliers module

---
+++ dev/src/bussines_layer/Branch.java
@@ -0,0 +1,14 @@
+package bussines_layer;
+
+import bussines_layer.inventory_module.Inventory;
+import bussines_layer.supplier_module.SupplierModule;
+
+public class Branch {
+    //fields:
+    private Integer branch_id;
+    private Inventory inventory;
+    private SupplierModule supplierModule;
+
+}
+
+
