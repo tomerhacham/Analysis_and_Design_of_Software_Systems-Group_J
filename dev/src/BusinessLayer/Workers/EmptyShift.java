5924bf995bfc59d91ef61ae228bbdbb0137a19f1
Einav Kaplan <einavkap@post.bgu.ac.il>
2020-05-13 13:17:39 +0300

    maven project

--- dev/src/BusinessLayer/Workers/EmptyShift.java
+++
@@ -1,9 +0,0 @@
-package BusinessLayer.Workers;
-import java.util.Date;
-import java.util.List;
-
-public class EmptyShift extends Shift {
-    public EmptyShift( Date date, boolean timeOfday) {
-        super(null, date, timeOfday);
-    }
-}
