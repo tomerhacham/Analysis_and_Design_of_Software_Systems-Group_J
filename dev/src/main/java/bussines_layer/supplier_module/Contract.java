f0042a347710ba31287ff178b4edf824ce91cb2c
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-05-13 19:44:41 +0300

    Immigration to Result design pattern in contract

--- dev/src/main/java/bussines_layer/supplier_module/Contract.java
+++ dev/src/main/java/bussines_layer/supplier_module/Contract.java
@@ -1,4 +1,5 @@
 package bussines_layer.supplier_module;
+import bussines_layer.Result;
 import bussines_layer.SupplierCard;
 import bussines_layer.inventory_module.CatalogProduct;
 import bussines_layer.inventory_module.GeneralProduct;
@@ -38,9 +39,62 @@ public class Contract {
     public SupplierCard getSupplierCard() {return supplier; }
 
 
-//#region Categories
+    //#region Categories
+    public LinkedList<String> getCategory() {
+        return categories;
+    }
+    public void setCategory(LinkedList<String> category) {
+        this.categories = category;
+    }
 
-    public boolean isCategoryExist (String category){
+    /**
+     * add category to the contract
+     * @param category
+     */
+    public Result addCategory(String category) {
+        Result result;
+        if (isCategoryExist(category)) {
+            result=new Result(false,category, String.format("Category: %s already exist in contract ID:%d", category,contractID));
+        }
+        else{
+            categories.add(category);
+            result=new Result(true, category, String.format("Category %s has been added to contract ID:%d", category,contractID));
+        }
+        return result;
+        }
+
+    /**
+     * removes category from the contract
+     * @param category
+     */
+    public Result removeCategory(String category){
+        Result result;
+        if (!isCategoryExist(category)) {
+            result=new Result(false, category, String.format("Could not find category: %s in contract ID:%d", category,contractID));
+        }
+        else {
+            categories.remove(category);
+            //remove all product from this category from products list
+            LinkedList<CatalogProduct> toRemove = new LinkedList<>();
+            for (Integer i : products.keySet()) {
+                if (products.get(i).getSupplierCategory().equals(category)) {
+                    toRemove.add(products.get(i));
+                }
+            }
+            for (CatalogProduct p : toRemove) {
+                removeProduct(p);
+            }
+            result=new Result(true,category, String.format("Category: %s has beem removed from contract ID:%d", category,contractID));
+        }
+        return result;
+    }
+
+    /**
+     * Utility function which checks if the category in the contract
+     * @param category
+     * @return
+     */
+    private boolean isCategoryExist (String category){
         for (String c : categories) {
             if(c.equals(category)){
                 return true;
@@ -48,189 +102,204 @@ public class Contract {
         }
         return false;
     }
+    //#endregion
 
-    public LinkedList<String> getCategory() {
-        return categories;
-    }
+    //#region Products
 
-    public void setCategory(LinkedList<String> category) {
-        this.categories = category;
-    }
-
-    public void addCategory(String c) {
-        if (isCategoryExist(c)) {
-            //sz_Result.setMsg("The Category IS Already In The Contract");  //TODO RESULT
-            return;
-        }
-        categories.add(c);
-    }
-
-    public void removeCategory(String c){
-        if (!isCategoryExist(c)) {
-            //sz_Result.setMsg("The Category dose not Exist");  //TODO RESULT
-            return;
-        }
-        categories.remove(c);
-        //remove all product from this category from products list
-        LinkedList <CatalogProduct> toRemove = new LinkedList<>();
-        for (Integer i : products.keySet()){
-            if (products.get(i).getSupplierCategory().equals(c) ){
-                toRemove.add(products.get(i));
+    /**
+     * reutrn all the catalogProduct under this contract
+     * @return
+     */
+    public Result getProducts() {
+        Result result;
+            if (products.isEmpty()){
+                result = new Result(false,null, String.format("There is no catalog product under contract %d", this.contractID));
             }
+            else {
+                LinkedList<CatalogProduct> listP = new LinkedList<>();
+                for (Integer i : products.keySet()) {
+                    listP.add(products.get(i));
+                }
+                result=new Result(true, listP, String.format("List of all the catalog product under contract ID: %d", this.contractID));
+            }
+            return result;
         }
-        for (CatalogProduct p : toRemove){
-            removeProduct(p);
-        }
 
-    }
-
-//#endregion
-
-//#region Products
-
-    public LinkedList<CatalogProduct> getProducts() {
-        if (products.isEmpty()){
-            //sz_Result.setMsg("The Supplier Has No Products");  //TODO result
-        }
-        LinkedList<CatalogProduct> listP = new LinkedList<>();
-        for (Integer i : products.keySet()){
-            listP.add(products.get(i));
-        }
-        return listP;
-    }
-
-    public void setProducts(HashMap<Integer, CatalogProduct> products) {
+    /**
+     * set all catalog product under this contract
+     * @param products
+     * @return
+     */
+    public Result setProducts(HashMap<Integer, CatalogProduct> products) {
         this.products = products;
+        return new Result(true, this, String.format("Product has been added under contract ID: %d", contractID));
     }
 
-    public void addProduct(CatalogProduct p){
-        // first check if the supplier can supply this category (check if the category is in the category list)
-        boolean categoryInList = isCategoryExist(p.getSupplierCategory());
-
-        if (categoryInList){
-            if (products.containsKey(p.getGpID())){
-                //sz_Result.setMsg("The Product Is Already In Your Product List");  //TODO RESULT
-            }
-            else{
-                products.put(p.getGpID() , p);
-            }
-        }
-        else{
-            //sz_Result.setMsg("The Category Of This Product Is Not In The Category List ");  //TODO RESULT
-        }
-    }
-
-    public void removeProduct(CatalogProduct product){
-        if (!products.containsKey(product.getGpID())){
-            //sz_Result.setMsg("There's no such Product on Contract"); //TODO RESULT
-            return;
-        }
-        products.remove(product.getGpID()); // remove product
-    }
-
-    public boolean isProductExist(CatalogProduct product){
-        if (products.containsKey(product.getGpID()))
-            return false;
-        else
-            return true;
-    }
-
-
-    public float getProductPrice(Integer productID){
-        if (products.containsKey(productID)){
-            return products.get(productID).getSupplierPrice();
-        }
-        //sz_result ("The Product Is Not In The Contract");  //TODO- result
-        return Integer.MAX_VALUE;
-    }
-
-
-//#endregion
-
-//#region CostEngineering
-
-//    public boolean isProductExist (Integer catalogID){
-//        for (Integer pid : products.keySet()){
-//            if (products.get(pid).getCatalogID() == catalogID){
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-
-    //TODo - update the changes
-    public boolean isProductExist (Integer id , boolean isCatalogid){
-        if(isCatalogid){
-            for (Integer pid : products.keySet()){
-                if (products.get(pid).getCatalogID().equals(id)){
-                    return true;
+    /**
+     * add new Catalog product to the product under this contract
+     * @param catalogProduct
+     * @return
+     */
+    public Result addProduct(CatalogProduct catalogProduct){
+        Result result;
+            // first check if the supplier can supply this category (check if the category is in the category list)
+            boolean categoryInList = isCategoryExist(catalogProduct.getSupplierCategory());
+            if (categoryInList){
+                if (products.containsKey(catalogProduct.getGpID())){
+                    result=new Result(false, catalogProduct, String.format("Catalog product already under contract ID:%d", contractID));
+                }
+                else{
+                    products.put(catalogProduct.getGpID() , catalogProduct);
+                    result=new Result(true,catalogProduct, String.format("%s has been added to contract ID:%d", catalogProduct,contractID));
                 }
             }
+            else{
+                result=new Result(false,catalogProduct, String.format("The Category Of %s Is Not In The Category List under contract ID:%d", catalogProduct,contractID));
+            }
+            return result;
+        }
+
+    /**
+     * remove catalog product from the product under this contracr
+     * @param catalogProduct
+     * @return
+     */
+    public Result removeProduct(CatalogProduct catalogProduct){
+        Result result;
+        if (!products.containsKey(catalogProduct.getGpID())){
+            result=new Result(false,catalogProduct, String.format("Could not find %s under contract ID:%d", catalogProduct,contractID));
+        }
+        else {
+            products.remove(catalogProduct.getGpID()); // remove product
+            result=new Result(true, catalogProduct, String.format("%s has been remove from contract ID:%d", catalogProduct,contractID));
+        }
+        return result;
+        }
+
+    /**
+     *checks if the product is under this contract
+     * @param product
+     * @return
+     */
+    public Result isProductExist(CatalogProduct product){
+        Result result;
+        if (products.containsKey(product.getGpID()))
+            result=new Result(true, product, String.format("Found %s", product));
+        else
+            result=new Result(false,product, String.format("Could not find %s under contract ID:%d", product,contractID));
+        return result;
+    }
+
+    /**
+     * return supplier price for the catalog product associate with the productID
+     * @param productID
+     * @return
+     */
+    public Result getProductPrice(Integer productID){
+        Result result;
+        if (products.containsKey(productID)){
+            Float supplier_price=products.get(productID).getSupplierPrice();
+            result=new Result(true,supplier_price, String.format("Supplier price for catalog product:%d", productID));
+        }
+        else{
+            result=new Result(false, productID, String.format("Could not find product:%d under contract ID:%d", productID,contractID));
+        }
+        return result;
+    }
+
+    //#endregion
+
+    //#region CostEngineering
+
+        //TODo - update the changes
+        private boolean isProductExist (Integer id , boolean isCatalogid){
+            if(isCatalogid){
+                for (Integer pid : products.keySet()){
+                    if (products.get(pid).getCatalogID().equals(id)){
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            if(products.containsKey(id)){
+                return true;
+            }
             return false;
         }
 
-        if(products.containsKey(id)){
-            return true;
+        public int getProductCatalogID(Integer productID){
+
+            return products.get(productID).getCatalogID();
         }
-        return false;
-    }
 
-    public int getProductCatalogID(Integer productID){
-
-        return products.get(productID).getCatalogID();
-    }
-
-    public void addCostEngineering() {
+    /**
+     * add cost engineering to this contract
+     */
+    public Result addCostEngineering() {
+        Result result;
         if (costEngineering != null){
-            //sz_result ("There is already CostEngineering to the Supplier");  //TODO- result
+            result=new Result(false,null, String.format("There is already cost engineering associate to contract ID:%d", contractID));
         }
-        this.costEngineering = new CostEngineering();
+        else {
+            this.costEngineering = new CostEngineering();
+            result=new Result(true, this.costEngineering, String.format("Cost engineering has been associate to contract ID:%d.", contractID));
+        }
+        return result;
     }
 
-    public void removeCostEngineering() {
+    /**
+     * remove the cost engineering associate with the contract
+     */
+    public Result removeCostEngineering() {
         costEngineering = null;
+        return new Result(true, null, String.format("Cost engineering has benn removed from contract ID:%d", contractID));
     }
 
+    /**
+     * return cost engineering
+     * @return
+     */
     public CostEngineering getCostEngineering() {
-        return costEngineering;
-    }
-
-    //update min quantity
-    public void updateMinQuantity(int catalogid , int minQuantity){
-        if (!isProductExist(catalogid , true)){
-            //sz_Result ("Product does not exist in the Contract" )  //TODO result
+            return costEngineering;
         }
-       costEngineering.updateMinQuantity(catalogid , minQuantity);
-    }
 
-    //update new price with sale
-    public void updatePriceAfterSale(int catalogid , int price){
-        costEngineering.updatePriceAfterSale(catalogid , price);
-    }
-
-    //add product to cost engineering
-    public void addProductToCostEng(int catalogid , int minQuantity , int price){
-        //check if the product is in the suppliers product list
-        if(isProductExist(catalogid , true)){
-            costEngineering.addProduct(catalogid , minQuantity , price);
+        //update min quantity
+        public void updateMinQuantity(int catalogid , int minQuantity){
+            if (!isProductExist(catalogid , true)){
+                //sz_Result ("Product does not exist in the Contract" )  //TODO result
+            }
+           costEngineering.updateMinQuantity(catalogid , minQuantity);
         }
-        else{
-            //sz_Result.setMsg("The Product Is Not In The Product list");   //TODO result
+
+        //update new price with sale
+        public void updatePriceAfterSale(int catalogid , int price){
+            costEngineering.updatePriceAfterSale(catalogid , price);
         }
-    }
 
-    //delete product from cost engineering
-    public void removeProductFromCostEng(int catalogid){
-        costEngineering.removeProduct(catalogid);
-    }
+        //add product to cost engineering
+        public void addProductToCostEng(int catalogid , int minQuantity , int price){
+            //check if the product is in the suppliers product list
+            if(isProductExist(catalogid , true)){
+                costEngineering.addProduct(catalogid , minQuantity , price);
+            }
+            else{
+                //sz_Result.setMsg("The Product Is Not In The Product list");   //TODO result
+            }
+        }
+
+        //delete product from cost engineering
+        public void removeProductFromCostEng(int catalogid){
+            costEngineering.removeProduct(catalogid);
+        }
 
 
-    public boolean isCostEngExist(){
-        return costEngineering!=null;
-    }
+        public boolean isCostEngExist(){
+            return costEngineering!=null;
+        }
 
 
-//#endregion
+    //#endregion
 
 }
 
