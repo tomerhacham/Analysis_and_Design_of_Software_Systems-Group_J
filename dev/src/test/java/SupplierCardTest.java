47b08f19743bd652de0d105c440e1725fb1e0c07
ZoeEllins <zoezellins@gmail.com>
2020-06-23 21:10:54 +0300

    add test directory

--- dev/src/test/java/SupplierCardTest.java
+++ dev/src/test/java/SupplierCardTest.java
@@ -1,89 +1,68 @@
-package java;
 import bussines_layer.Branch;
+import bussines_layer.BranchController;
 import bussines_layer.SupplierCard;
 import bussines_layer.SupplierController;
 import bussines_layer.enums.supplierType;
 import bussines_layer.inventory_module.CatalogProduct;
+import bussines_layer.inventory_module.Category;
 import bussines_layer.inventory_module.GeneralProduct;
+import bussines_layer.inventory_module.ProductController;
 import bussines_layer.supplier_module.Contract;
+import data_access_layer.Mapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import presentation_layer.CLController;
+
 import java.util.LinkedList;
 import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertNotEquals;
 
 
 public class SupplierCardTest {
-    Branch branch = new Branch(2 , "BeerSheva");
-    SupplierController supplierController= SupplierController.getInstance();
-    SupplierCard supplierCard;
-    GeneralProduct gp ;
-    GeneralProduct gp3;
-    CatalogProduct p;
-    CatalogProduct p3;
+    BranchController branchController = new BranchController(true);
+    Mapper mapper = Mapper.getInstance();
+    SupplierCard halavi_Lee = mapper.find_Supplier(1);
+    Contract contract = mapper.find_Contract(1 ,1);
 
     @BeforeEach
     public void setUp(){
-        LinkedList<String> contact = new LinkedList();
-        contact.add("Moshe");
-        contact.add("Rachel");
-        LinkedList<String> category = new LinkedList<>();
-        category.add("Dairy");
-        category.add("Vegetables");
-
-        supplierCard = new SupplierCard("halavi-lee" , "ringelbloom 97, beer-sheva" , "halavi@gmail.com" , "08- 1234567" , 14458727  , "0975635" , "CreditCard" , contact , supplierType.byOrder );
-
-        gp = new GeneralProduct("Tnuva" , "milk" , (float)7.0 , (float)6.0 , 5 , 12 , 121 , 14458727 , "Dairy" , 2 , (float) 1.0);
-
-        p = new CatalogProduct(2 , 12 ,121 , 7.0 ,14458727, "milk" , 7 , "Tnuva" , "Dairy" , 27784);
-        Integer branch_id,Integer catalogID, Integer gpID, Float supplier_price, Integer supplier_id, String supplier_category , String name , Float weight) {
-        p3 = new CatalogProduct(18 , "Pepper" , 8 , "Meshek77" , "Vegetables" , 27718);
-
-        supplierController.addProductToContract(14458727 ,p);
-        supplierController.addProductToContract(14458727 ,p3);
-
-
-        Contract contract = new Contract(category , 14458727 , "by Order");
-
-        SupplierCard supplier , Integer contractID , Integer branchID , LinkedList<String> categories){
-
-        }
+        CLController.initialize();
+        branchController.switchBranch(1);
+    }
 
     @AfterEach
     public void tearDown(){
-        supplierController.deleteSupplierCard(14458727);
+        branch.removeContract(supplierCard);
     }
 
     @Test
     public void testName(){
         try{
-            supplierController.ChangeSupplierName(14458727 , "Zoe");
+            halavi_Lee.setSupplierName("zoe");
         }catch (Exception e){
             fail("Exception "+ e);
         }
 
-        assertEquals(supplierCard.getSupplierName(),"Zoe");
-        assertNotEquals(supplierCard.getSupplierName(), "halavi-lee");
+        assertEquals(halavi_Lee.getSupplierName(),"Zoe");
+        assertNotEquals(halavi_Lee.getSupplierName(), "halavi-Lee");
     }
 
     @Test
     public void testAddress(){
         try{
-            supplierController.ChangeAddress(14458727 ,"TelAviv");
+            halavi_Lee.setAddress("TelAviv");
         }catch (Exception e){
             fail("Exception "+ e);
         }
-        assertEquals(supplierCard.getAddress(),"TelAviv");
-        assertNotEquals(supplierCard.getAddress(), "ringelbloom 97, beer-sheva");
+        assertEquals(halavi_Lee.getAddress(),"TelAviv");
+        assertNotEquals(halavi_Lee.getAddress(), "ringelbloom 97, beer-sheva");
     }
 
     @Test
     public void testAddCategory(){
-        Contract contract= supplierCard.getContract();
-
         try{
-            supplierController.addCategory(14458727 ,"Meat");
+            contract.addCategory("Meat");
         }catch (Exception e){
             fail("Exception "+ e);
         }
@@ -92,9 +71,8 @@ public class SupplierCardTest {
 
     @Test
     public void testDeleteCategory(){
-        Contract contract = supplierCard.getContract();
         try{
-            supplierController.deleteCategory(14458727 ,"Dairy");
+            contract.removeCategory("Dairy");
         }catch (Exception e){
             fail("Exception "+ e);
         }
@@ -102,47 +80,17 @@ public class SupplierCardTest {
     }
 
     @Test
-    public void testAddProductToContract(){
-
-        Product p2 = new Product(15 , "yogurt" , 12 , "Tnuva" , "Dairy" , 27845);
-        assertFalse(supplierCard.getContract().getProducts().isEmpty());
-        assertFalse(supplierCard.getContract().getProducts().contains(p2));
-        supplierController.addProductToContract(14458727 ,p2);
-        assertTrue(supplierCard.getContract().getProducts().contains(p2));
-
-    }
-
-    @Test
     public void testDeleteProductFromContract(){
 
-        assertFalse(supplierCard.getContract().getProducts().isEmpty());
-
+        assertFalse(contract.getProducts().getData().isEmpty());
+        CatalogProduct cp = mapper.find_CatalogProduct(10 , 1);
         try{
-            supplierController.deleteProduct(14458727 ,p3.getCatalogID());
+            contract.removeProduct(cp);
         }catch (Exception e){
             fail("Exception "+ e);
         }
-
-        assertFalse(supplierCard.getContract().getProducts().contains(p3));
+        assertFalse(contract.getProducts().getData().contains(cp));
     }
-
-    @Test
-    public void testAddProductToCostEngineering(){
-
-        Product p4 = new Product(27 , "Banana" , 15 , "Meshek77" , "Vegetables" , 27568);
-        supplierController.addProductToContract(14458727 ,p4);
-        supplierController.createCostEngineering(14458727);
-        try{
-            supplierController.addProductToCostEng(14458727,27568 , 20 , 5);
-        }catch (Exception e){
-            fail("Exception "+ e);
-        }
-        assertFalse(supplierCard.getCostEngineering().getMinQuntity().isEmpty());
-        assertTrue(supplierCard.getCostEngineering().getMinQuntity().containsKey(27568));
-        assertEquals(new Integer(20) , supplierCard.getCostEngineering().getMinQuntity().get(27568));
-
-    }
-
 
 
 }
