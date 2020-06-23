0dbce64251acb8a2441f0f837831f7792ebc6621
Shira Sarig <sarigshi@post.bgu.ac.il>
2020-06-23 18:32:04 +0300

    tests truck

---
+++ dev/test/TruckControllerTest.java
@@ -0,0 +1,43 @@
+package bussines_layer.transport_module;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class TruckControllerTest {
+
+    TruckController truckController;
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+    public TruckControllerTest(TruckController t_c){
+        truckController = t_c;
+    }
+
+    @Test
+    public void createTruck() {
+        // max weight smaller than net weight
+        assertFalse(truckController.CreateTruck("WE-1234-FD", "RNO51", 3000, 2500, "A"));
+
+        // max weight equal net weight
+        assertFalse(truckController.CreateTruck("WE-1234-FD", "RNO51", 3000, 3000, "A"));
+    }
+
+    @Test
+    public void deleteTruck() {
+        assertTrue(truckController.DeleteTruck(1));
+    }
+
+    @Test
+    public void checkIfTrucksAvailableByDate() {
+        Date d1;
+        try {
+            d1 = formatter.parse("07/07/2020");
+            assertTrue(truckController.checkIfTrucksAvailableByDate(d1, false));
+        } catch (Exception e) {
+            System.out.println("Test - checkIfTrucksAvailableByDate - Failed.");
+        }
+    }
+}
